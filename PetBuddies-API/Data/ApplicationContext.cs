03e61798e8719c1c0d116f5848f73fbf3315330c
gabriel <spbiel18@gmail.com>
2026-05-19 21:56:47 -0300

    feat: adding OnModelCreating mapping enums to string and bools to NUMBER(1) and regenerating migration

--- PetBuddies-API/Data/ApplicationContext.cs
+++ PetBuddies-API/Data/ApplicationContext.cs
@@ -9,6 +9,26 @@ namespace PetBuddies_API.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (property.ClrType.IsEnum)
+                    {
+                        property.SetProviderClrType(typeof(string));
+                        property.SetMaxLength(50);
+                    }
+
+                    if (property.ClrType == typeof(bool) || property.ClrType == typeof(bool?))
+                        property.SetColumnType("NUMBER(1)");
+                }
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
             var now = DateTime.Now;
@@ -21,6 +41,8 @@ namespace PetBuddies_API.Data
             }
             return base.SaveChangesAsync(cancellationToken);
         }
+
+
 
         public DbSet<AnimalEntity> Animais { get; set; }
         public DbSet<ClinicaEntity> Clinicas { get; set; }
